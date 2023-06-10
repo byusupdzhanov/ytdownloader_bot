ba847c1d2918c0209d6dd4c166ad6e4ff97cd795
Bogdan Yusupdzhanov <97046736+getsucked@users.noreply.github.com>
2023-06-10 21:19:13 +0300

    added new api's

---
+++ config.py
@@ -0,0 +1,9 @@
+bot_settings = {
+    'token': '6077264956:AAFsYAsEekCmoYUYBn_XultvUDWWbE_YR34',
+    'api_id': '26793093',
+    'api_hash': '4a18c365b31836b1dcff541ccb84e367',
+}
+
+img_src = {
+    'uc_photo': 'https://s503sas.storage.yandex.net/rdisk/3b7352c1130fd38ace2251803803d1b31be892077b62370781ac087f922067c7/64163b5e/xAT9F320_wuTumXz8U_X7jHrNV7ehMUt8EKYdUM836HPY3vk2vz1kmdCoXYHHATK1ONMCwXCb4JIHtP49FgMkg==?uid=797670624&filename=under-construction-sign-with-hat-on-traffic-cone-vector.webp&disposition=inline&hash=&limit=0&content_type=image%2Fwebp&owner_uid=797670624&fsize=59546&hid=7ca551be38cede32b02383b172daf8f4&media_type=image&tknv=v2&etag=0db69dc6f99ecfa69708a30ea7df72c1&rtoken=1Zs5VB17M0ZL&force_default=yes&ycrid=na-1103900fb49c2b82ab663dd8fd7b65b6-downloader7e&ts=5f7343b5f1380&s=5573e9786b94823ec39be029a36c0acaf37e1f1b15f43d243f75164e0a13e70e&pb=U2FsdGVkX19mUz88pKLAarRBc3f_wsvc3l5fwS4v1m8E3LbV_SR4o16JtAPPZB18o6yXXAJlEN2IQYM2LSKdBiMX3usg4r1C-mV9I3xpPU8',
+}
