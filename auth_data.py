b24227d1d4f92a99406a9eb1a418283254455815
Bogdan Yusupdzhanov <97046736+getsucked@users.noreply.github.com>
2023-06-28 18:30:58 +0300

    removed test api's

---
+++ auth_data.py
@@ -0,0 +1,5 @@
+bot_settings = {
+    'token': 'yout telegram bot token', #get from BotFather,
+    'api_id': 'xxxx', #for test, not in production
+    'api_hash': 'xxx', #for test, not in production
+}
