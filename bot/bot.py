28955af7808f8feebe6425e9073d96cd2aca1b06
Bogdan Yusupdzhanov <97046736+getsucked@users.noreply.github.com>
2023-07-11 23:46:09 +0500

    bump for v.1.1.2

--- bot/bot.py
+++ bot/bot.py
@@ -14,8 +14,12 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from moviepy.editor import VideoFileClip
 import re
+import requests
+import youtube_dl
+
 
 bot = AsyncTeleBot(token=bot_settings['token'], state_storage=StateMemoryStorage())
+
 
 # учет пользвателей в GSheets
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -25,13 +29,16 @@ client = gspread.authorize(creds)
 sheet = client.open_by_key(sheet_id).sheet1
 
 urls = {}
+params = []
+users_params = {}
 
 class MyStates(StatesGroup):
     obtainingUrl = State()
     MediaFormatChoosing = State()
     downloadHandler = State()
     mainMenu = State()
-
+    vkDownloading = State()
+    vkQualityHandler = State()
 
 class ProductsCallbackFilter(AdvancedCustomFilter):
     key = 'config'
@@ -57,6 +64,8 @@ def backrepeat_keyboard():
                                       )
 
 
+@bot.message_handler(commands=[''])
+
 @bot.message_handler(commands=['start'])
 async def start_message(message):
     chat_id = message.chat.id
@@ -66,8 +75,106 @@ async def start_message(message):
 
 
 @bot.message_handler(commands=['vk'])
-async def vk_first_msg(message):
-    await bot.send_photo(message.chat.id, photo=img_src['uc_photo'], caption=texts['notready'], parse_mode='html')
+async def handle_vk_command(message):
+    await bot.send_message(message.chat.id, text=texts['vk_start_text'], parse_mode='html')
+    await bot.set_state(message.from_user.id, MyStates.vkQualityHandler, message.chat.id)
+
+
+@bot.message_handler(state=MyStates.vkQualityHandler)
+async def process_vk_link(message):
+    link = message.text
+    chat_id = message.chat.id
+    params.append(link)
+    # Здесь вы можете добавить проверку введенной ссылки и обработку ошибок, если необходимо
+
+    # Создание объекта клавиатуры
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    # Добавление кнопок
+    button_480 = types.KeyboardButton('480')
+    button_360 = types.KeyboardButton('360')
+    button_720 = types.KeyboardButton('720')
+    keyboard.add(button_480, button_360, button_720)
+
+    await bot.send_message(chat_id, 'На данный момент, возможна загрузка только видео🙃\n\n'
+                                    'Выберите качество загружаемого видео📹:', reply_markup=keyboard)
+    await bot.set_state(message.from_user.id, MyStates.vkDownloading, message.chat.id)
+
+
+@bot.message_handler(state=MyStates.vkDownloading)
+async def process_vk_video(message):
+    try:
+        async def downloadsend(video_url, message, token, owner_id, quality):
+            await bot.send_message(chat_id=message.chat.id, text='Идет скачивание, ожидайте')
+            video_get_url = f"https://api.vk.com/method/video.get?owner_id={owner_id}&videos={video_url}" \
+                            f"&access_token={token}&v=5.81"
+            req = requests.get(video_get_url).json()
+            response = req["response"]
+            items = response['items'][0]
+            if 'content_restricted_message' in items:
+                await bot.send_message(chat_id=message.chat.id, text=texts['age_error'])
+                return handle_vk_command(message)
+            duration = items['duration']
+
+            widthvideo = items['width']
+            heightvideo = items['height']
+            url = items['player']
+            ydl_opts = {
+                'format': 'best[ext=mp4]/best',
+                'outtmpl': f'%(title)s_{video_url}.%(ext)s',
+                'self_contained': True
+            }
+
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=True)
+                video_filename = ydl.prepare_filename(info_dict)
+                video_file_size = os.path.getsize(video_filename)
+                video = VideoFileClip(video_filename)
+                video_resized = video.resize(height=quality)
+                # Изменение разрешения до 720p
+                max_video_size = 50 * 1024 * 1024  # 50 МБ (максимальный размер видео в Telegram)
+                video_resized.write_videofile('resized_video.mp4', codec='libx264')
+                video_file_size = os.path.getsize('resized_video.mp4')
+                os.remove(video_filename)
+                if video_file_size < max_video_size:
+                    with open('resized_video.mp4', 'rb') as video_file:
+                        await bot.send_video(chat_id=message.chat.id,
+                                       video=video_file,
+                                       caption='Скачано при помощи \n@getsdownload_bot ✅',
+                                       duration=int(duration),
+                                       supports_streaming=True,
+                                       width=int(widthvideo),
+                                       height=int(heightvideo))
+                    params.clear()
+                    os.remove('resized_video.mp4')
+                else:
+                    await bot.send_message(message.chat.id, text=texts['limitation_text_with_q'],
+                                           parse_mode='html')
+                    await handle_vk_command(message)
+
+        quality = int(message.text)
+        video_url = params[0]
+        token = bot_settings['vk_token']
+        owner_id = bot_settings['vk_owner_id']
+
+        if 'https://vk.com/video-' in video_url:
+            video_url = video_url[20:]
+        elif 'https://vk.com/video' in video_url and '-' not in video_url:
+            video_url = video_url[20:]
+        elif 'https://vk.com/clip-' in video_url:
+            video_url = video_url[19:]
+        elif 'https://vk.com/clip' in video_url and '-' not in video_url:
+            video_url = video_url[19:]
+        else:
+            await bot.send_message(message.chat.id, "Это не ссылка на видео VK.")
+            return handle_vk_command(message)
+
+        if '?' in video_url:
+            video_url = video_url.split('?')[0]
+
+        await downloadsend(video_url, message, token, owner_id, quality)
+    except Exception as e:
+        await bot.send_message(message.chat.id, f'Произошла непредвиденная ошибка: {str(e)}')
+        await bot.send_message(chat_id='-1001879360469', text=str(e))
 
 
 @bot.message_handler(commands=['tiktok'])
@@ -170,9 +277,11 @@ async def youtube_media_downloading(message):
             elif 'object has no attribute' in str(e):
                 await bot.send_message(message.chat.id, text=texts['error'],
                                        reply_markup=backrepeat_keyboard(), parse_mode='html')
+                await bot.send_message(chat_id='-1001879360469', text=str(e))
             else:
                 await bot.send_message(message.chat.id, f'Непредвиденная ошибка: {str(e)}',
                                        reply_markup=backrepeat_keyboard())
+                await bot.send_message(chat_id='-1001879360469', text=str(e))
     elif text == 'Аудио🎵':
         await bot.send_message(message.chat.id, 'Идет скачивание, ожидайте!')
         youtube = YouTube(url)
@@ -195,10 +304,11 @@ async def youtube_media_downloading(message):
             else:
                 await bot.send_message(message.chat.id, f'Непредвиденная ошибка: {str(e)}',
                                        reply_markup=backrepeat_keyboard())
+                await bot.send_message(chat_id='-1001879360469', text=str(e))
         # os.remove(filename)
 
 
 bot.add_custom_filter(asyncio_filters.StateFilter(bot))
 bot.add_custom_filter(ProductsCallbackFilter())
 
-asyncio.run(bot.polling(none_stop=True))
+asyncio.run(bot.polling(non_stop=True))
