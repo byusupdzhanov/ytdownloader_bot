28b78c3152720b88b6ba7874449ee983b7a48c13
Bogdan Yusupdzhanov <97046736+getsucked@users.noreply.github.com>
2023-07-13 16:19:29 +0500

    bump for v1.1.2

--- auth_data.py
+++ auth_data.py
@@ -2,4 +2,6 @@ bot_settings = {
     'token': 'yout telegram bot token', #get from BotFather,
     'api_id': 'xxxx', #for test, not in production
     'api_hash': 'xxx', #for test, not in production
+    'vk_token': 'user token', #get it on https://dev.vk.com
+    'owner_id': 'your vk account id' #only number, e.g. 1939294
 }
